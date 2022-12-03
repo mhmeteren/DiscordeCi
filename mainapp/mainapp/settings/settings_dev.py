b1143cfa8f22771127c51eec0dc4bde9a1196ca5
mehmetsolucatutan <mehmetsolucatutan@gmail.com>
2022-12-03 23:27:40 +0300

    update settings

---
+++ mainapp/mainapp/settings/settings_dev.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'altYapiDatabase',
+        'USER': 'root',
+        'PASSWORD': 'test123',
+        'HOST': 'localhost',   
+        'PORT': '3306',
+    }
+}
